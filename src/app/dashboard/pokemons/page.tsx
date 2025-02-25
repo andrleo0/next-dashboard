b3d74888ab626db08e288d24c8a54386fada1eb3
Andres Guerrero <aguerrero@fi.uba.ar>
2025-02-25 20:51:29 -0300

    add pokemon card

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,8 +1,33 @@
+import { PokemonGrid, PokemonsResponse, SimplePokemon } from "@/app/pokemons";
 
-export default function PokemonsPage() {
+const getPokemos = async (limit = 20, offset = 0): Promise<SimplePokemon[]> => {
+
+  const data: PokemonsResponse = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
+    .then(res => res.json());
+
+  const pokemons = data.results.map(pokemon => {
+    return {
+      id: pokemon.url.split('/').at(-2)!,
+      name: pokemon.name
+    }
+  });
+
+  return pokemons;
+}
+
+
+export default async function PokemonsPage() {
+
+  const pokemons = await getPokemos(151);
+
+
   return (
-    <div>
-      <h1>Hello Pokemons Page</h1>
+    <div className="flex flex-col">
+
+      <span className="text-5xl my-2">Listado de Pokémons <small>estático</small></span>
+
+      <PokemonGrid pokemons={pokemons} />
+
     </div>
   );
 }
